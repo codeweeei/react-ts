2ca26991bc1716df987aa0b3a928c3fd888e4ce2
codeweeei <1749894019@qq.com>
2023-06-24 14:58:24 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParent: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxBracketSameLine: false,
+    printWidth: 100,
+    proseWrap: 'preserve',
+    semi: false, // 去除分号
+    singleQuote: true, // 单引号
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    parser: 'typescript'
+}
